34d093cb9fdff5c1910ddc64023cfcc9b21b4a45
guiyanahaha <146899289+guiyanahaha@users.noreply.github.com>
2023-12-06 20:06:32 -0500

    Update Wall.ino

--- Wall.ino
+++ Wall.ino
@@ -1,44 +1,41 @@
-#define Motor_channel1 0 
-#define Motor_channel2 1 
-#define Motor_channel3 2
+const int PHOTOTRANSISTOR_PIN = 10;
+const int LED_RED = 19;
+const int LED_GREEN = 18;
 
-#define Motor_freq 50  //Motor frequency
-#define Motor_resolution_bits 12 //Motor resolution in bits
-#define Motor_resolution ((1<<Motor_resolution_bits)-1)
+static int old_time = 0;
+int frequency = 0;
+volatile uint32_t Down_time = 0;
 
-const int HeadPin = 1;  // GPIO pin for the Head Motor
+void IRAM_ATTR handleInterrupt1() {
+  Down_time = millis(); // millis returns ms since the program started.
+}
 
 void setup() {
   Serial.begin(115200);
-  
-  ledcSetup(Motor_channel1, Motor_freq, Motor_resolution_bits); // setup Motor1 channel1
-  ledcAttachPin(HeadPin, Motor_channel1); //motor 1
-}
-
-void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 4095) {
-  uint32_t duty = Motor_resolution * min(value, valueMax) / valueMax;
-  ledcWrite(channel, duty); // write duty to Motor pin
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(PHOTOTRANSISTOR_PIN, INPUT);  // Set the phototransistor pin as input
+  attachInterrupt(digitalPinToInterrupt(PHOTOTRANSISTOR_PIN), handleInterrupt1, FALLING);
 }
 
 void loop() {
-    ledcAnalogWrite(Motor_channel1,map(130, 0, 180, 122, 492));  // update Motor duty cycle
-    delay(100); 
-
-    ledcAnalogWrite(Motor_channel1,map(162, 0, 180, 122, 492));  // update Motor duty cycle
-    delay(300);
-
-    ledcAnalogWrite(Motor_channel1,map(130, 0, 180, 122, 492));  // update Motor duty cycle
-    delay(100);
-
-    ledcAnalogWrite(Motor_channel1,map(98, 0, 180, 122, 492));  // update Motor duty cycle
-    delay(100);      
-
-    ledcAnalogWrite(Motor_channel1,map(66, 0, 180, 122, 492));  // update Motor duty cycle
-    delay(100);
-    
-    ledcAnalogWrite(Motor_channel1,map(34, 0, 180, 122, 492));  // update Motor duty cycle
-    delay(100);
-
-    ledcAnalogWrite(Motor_channel1,map(98, 0, 180, 122, 492));  // update Motor duty cycle
-    delay(300); 
+  frequency = Down_time - old_time;
+  old_time = Down_time;
+  if (frequency >= 21 && frequency <= 25) {
+        digitalWrite(LED_RED, HIGH); // Turn on the red LED
+        digitalWrite(LED_GREEN, LOW); // Turn off the green LED
+        Serial.print("Frequency: ");
+        Serial.println(frequency);
+    } else if (frequency >= 540 && frequency <= 560){
+        digitalWrite(LED_RED, LOW); // Turn off the red LED
+        digitalWrite(LED_GREEN, HIGH);  // Turn on the green LED
+        Serial.print("Frequency: ");
+        Serial.println(frequency);
+    }else {
+        digitalWrite(LED_RED, LOW); // Turn off all LED
+        digitalWrite(LED_GREEN, LOW);
+        Serial.print("Frequency: ");
+        Serial.println(frequency);
+    }
+  
 }
